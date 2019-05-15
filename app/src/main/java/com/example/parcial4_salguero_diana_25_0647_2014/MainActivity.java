78cd2af1f2c8605f6a4832857beee679a0d3a589
Toki1117 <diana.salguero11@outlook.com>
2019-05-15 07:54:15 -0600

    :sparkles: adding personal data fragment

--- app/src/main/java/com/example/parcial4_salguero_diana_25_0647_2014/MainActivity.java
+++ app/src/main/java/com/example/parcial4_salguero_diana_25_0647_2014/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.parcial4_salguero_diana_25_0647_2014;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,7 +17,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        PersonalData.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,11 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        Fragment f = new PersonalData();
+        if (f != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_main, f).commit();
+        }
     }
 
     @Override
@@ -77,17 +85,32 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        boolean fragmentSeleccionado = false;
+
+        Fragment f = null;
 
         if (id == R.id.datos_personaless) {
-            // Handle the camera action
+            f = new PersonalData();
+            fragmentSeleccionado = true;
         } else if (id == R.id.preferencias) {
 
         } else if (id == R.id.salir) {
+            finish();
+        }
 
+        if (fragmentSeleccionado) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_main, f).commit();
+            item.setChecked(true);
+            getSupportActionBar().setTitle(item.getTitle());
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
